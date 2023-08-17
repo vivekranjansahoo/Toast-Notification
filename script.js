caddf5424a420fa05bc58db8c8a1eb7a4d4dc8a2
Vivek Ranjan Sahoo <97107034+vivekranjansahoo@users.noreply.github.com>
2023-08-17 11:12:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let toastBox = document.getElementById("toastBox");
+
+let successMsg = "Successfully submitted";
+let errorMsg = "please fix the error message";
+let invalidMsg = "Invalid input, check again";
+
+function showToast(msg) {
+  let toast = document.createElement("div");
+  toast.classList.add("toast");
+  toast.innerHTML = msg;
+  toastBox.appendChild(toast);
+
+  if (msg.includes("error")) {
+    toast.classList.add("error");
+  }
+  if (msg.includes("Invalid")) {
+    toast.classList.add("invalid");
+  }
+
+  setTimeout(() => {
+    toast.remove();
+  }, 6000);
+}
